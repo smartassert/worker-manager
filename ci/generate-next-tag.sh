e7f83e9ef1f504d2053d5eea8e513e10e508514c
Jon Cram <webignition@gmail.com>
2021-07-08 16:22:32 +0100

    Generate version if creating automatic release

---
+++ ci/generate-next-tag.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git fetch --tags --quiet
+LATEST_TAG=$(git tag | tail -1)
+MAJOR_VERSION=$(echo $LATEST_TAG | cut -d'.' -f1)
+MINOR_VERSION=$(echo $LATEST_TAG | cut -d'.' -f2)
+NEXT_MINOR_VERSION=$((MINOR_VERSION+1))
+NEXT_TAG="${MAJOR_VERSION}.${NEXT_MINOR_VERSION}"
+
+echo $NEXT_TAG
