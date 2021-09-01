df4c04c5c2065a4a3c1b234bde2ce481873b828b
Jon Cram <webignition@gmail.com>
2021-09-01 14:08:30 +0100

    Revert create-version-label.sh

--- ci/create-version-label.sh
+++ ci/create-version-label.sh
@@ -2,9 +2,9 @@
 
 git fetch --tags --quiet
 LATEST_TAG=$(git tag | tail -1)
-MAJOR_VERSION=$(echo "$LATEST_TAG" | cut -d'.' -f1)
-MINOR_VERSION=$(echo "$LATEST_TAG" | cut -d'.' -f2)
+MAJOR_VERSION=$(echo $LATEST_TAG | cut -d'.' -f1)
+MINOR_VERSION=$(echo $LATEST_TAG | cut -d'.' -f2)
 NEXT_MINOR_VERSION=$((MINOR_VERSION+1))
 VERSION="${MAJOR_VERSION}.${NEXT_MINOR_VERSION}"
 
-echo "$VERSION"
+echo $VERSION
