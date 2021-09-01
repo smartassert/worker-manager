833ca03819fd2b0f24c1bd1fdccce0b76b43936a
Jon Cram <webignition@gmail.com>
2021-09-01 14:26:11 +0100

    Fix just one shellcheck issue

--- ci/create-version-label.sh
+++ ci/create-version-label.sh
@@ -2,7 +2,7 @@
 
 git fetch --tags --quiet
 LATEST_TAG=$(git tag | tail -1)
-MAJOR_VERSION=$(echo $LATEST_TAG | cut -d'.' -f1)
+MAJOR_VERSION=$(echo "$LATEST_TAG" | cut -d'.' -f1)
 MINOR_VERSION=$(echo $LATEST_TAG | cut -d'.' -f2)
 NEXT_MINOR_VERSION=$((MINOR_VERSION+1))
 VERSION="${MAJOR_VERSION}.${NEXT_MINOR_VERSION}"
