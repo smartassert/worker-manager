fb80814ce204d2528e2a7735c6d8f63da6639861
Jon Cram <webignition@gmail.com>
2021-10-22 17:39:50 +0100

    Sort tags before picking latest (#113)

--- ci/scripts/create-version-label.sh
+++ ci/scripts/create-version-label.sh
@@ -2,7 +2,7 @@
 
 git fetch --tags --quiet
 
-LATEST_TAG=$(git tag | tail -1)
+LATEST_TAG=$(git tag | sort -V | tail -1)
 MAJOR_VERSION=$(echo "$LATEST_TAG" | cut -d'.' -f1)
 MINOR_VERSION=$(echo "$LATEST_TAG" | cut -d'.' -f2)
 NEXT_MINOR_VERSION=$((MINOR_VERSION+1))
