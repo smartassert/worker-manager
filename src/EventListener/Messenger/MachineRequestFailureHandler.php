1591bc7656cb99a89afd9524c28a9323f63bc3eb
Jon Cram <webignition@gmail.com>
2022-01-26 16:32:32 +0000

    Improve MachineRequestFailureHandler exception logging

--- src/EventListener/Messenger/MachineRequestFailureHandler.php
+++ src/EventListener/Messenger/MachineRequestFailureHandler.php
@@ -3,7 +3,6 @@
 namespace App\EventListener\Messenger;
 
 use App\Entity\Machine;
-use App\Exception\StackedExceptionInterface;
 use App\Message\CreateMachine;
 use App\Message\DeleteMachine;
 use App\Message\FindMachine;
@@ -11,7 +10,9 @@ use App\Message\GetMachine;
 use App\Message\MachineRequestInterface;
 use App\Services\Entity\Factory\CreateFailureFactory;
 use App\Services\Entity\Store\MachineStore;
-use SmartAssert\InvokableLogger\ExceptionLogger;
+use App\Services\MessageHandlerExceptionFinder;
+use App\Services\MessageHandlerExceptionStackFactory;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Messenger\Event\WorkerMessageFailedEvent;
 
@@ -20,7 +21,9 @@ class MachineRequestFailureHandler implements EventSubscriberInterface
     public function __construct(
         private MachineStore $machineStore,
         private CreateFailureFactory $createFailureFactory,
-        private ExceptionLogger $exceptionLogger,
+        private MessageHandlerExceptionFinder $messageHandlerExceptionFinder,
+        private MessageHandlerExceptionStackFactory $exceptionStackFactory,
+        private LoggerInterface $messengerAuditLogger,
     ) {
     }
 
@@ -52,18 +55,14 @@ class MachineRequestFailureHandler implements EventSubscriberInterface
             return;
         }
 
-        $throwable = $event->getThrowable();
-        if ($throwable instanceof StackedExceptionInterface) {
-            foreach ($throwable->getExceptionStack() as $exception) {
-                $this->exceptionLogger->log($exception);
-            }
-        } else {
-            $this->exceptionLogger->log($throwable);
+        $messageHandlerException = $this->messageHandlerExceptionFinder->findInWorkerMessageFailedEvent($event);
+        foreach ($this->exceptionStackFactory->create($messageHandlerException) as $loggableException) {
+            $this->logException($message, $loggableException);
         }
 
         if ($message instanceof CreateMachine) {
             $machine->setState(Machine::STATE_CREATE_FAILED);
-            $this->createFailureFactory->create($machine->getId(), $throwable);
+            $this->createFailureFactory->create($machine->getId(), $messageHandlerException);
         }
 
         if ($message instanceof DeleteMachine) {
@@ -79,5 +78,18 @@ class MachineRequestFailureHandler implements EventSubscriberInterface
         }
 
         $this->machineStore->store($machine);
+    }
+
+    private function logException(MachineRequestInterface $message, \Throwable $throwable): void
+    {
+        $this->messengerAuditLogger->critical(
+            $throwable->getMessage(),
+            [
+                'message_id' => $message->getUniqueId(),
+                'machine_id' => $message->getMachineId(),
+                'code' => $throwable->getCode(),
+                'exception' => $throwable::class,
+            ]
+        );
     }
 }
