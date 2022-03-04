18ee5d1387f0e851eba171a2be52f29662d12a51
Jon Cram <webignition@gmail.com>
2022-03-04 10:43:05 +0000

    Create migration for table for Machine entity

---
+++ migrations/Version20220304104056.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use App\Entity\Machine;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20220304104056 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create table for ' . Machine::class;
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('
+            CREATE TABLE machine (
+                id VARCHAR(32) NOT NULL, 
+                state VARCHAR(255) NOT NULL, 
+                ip_addresses TEXT DEFAULT NULL, 
+                PRIMARY KEY(id)
+            )
+        ');
+        $this->addSql('COMMENT ON COLUMN machine.ip_addresses IS \'(DC2Type:simple_array)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE machine');
+    }
+}
