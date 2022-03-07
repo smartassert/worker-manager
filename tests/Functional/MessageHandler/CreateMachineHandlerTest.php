100df516459f0e7bce559a521c2d6dba54dd31f5
Jon Cram <webignition@gmail.com>
2022-03-07 14:33:12 +0000

    Use DropletProxy in CreateMachineHandler tests

--- tests/Functional/MessageHandler/CreateMachineHandlerTest.php
+++ tests/Functional/MessageHandler/CreateMachineHandlerTest.php
@@ -20,17 +20,16 @@ use App\Services\Entity\Store\MachineProviderStore;
 use App\Services\Entity\Store\MachineStore;
 use App\Services\MachineRequestFactory;
 use App\Tests\AbstractBaseFunctionalTest;
+use App\Tests\Proxy\DigitalOceanV2\Api\DropletProxy;
 use App\Tests\Services\Asserter\MessengerAsserter;
-use App\Tests\Services\HttpResponseFactory;
 use App\Tests\Services\SequentialRequestIdFactory;
 use App\Tests\Services\TestMachineRequestFactory;
 use DigitalOceanV2\Entity\Droplet as DropletEntity;
 use DigitalOceanV2\Exception\ResourceNotFoundException;
-use DigitalOceanV2\Exception\RuntimeException;
-use GuzzleHttp\Handler\MockHandler;
-use GuzzleHttp\Psr7\Response;
+use DigitalOceanV2\Exception\RuntimeException as VendorRuntimeException;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
-use Psr\Http\Message\ResponseInterface;
+use SmartAssert\DigitalOceanDropletConfiguration\Configuration;
+use SmartAssert\DigitalOceanDropletConfiguration\Factory;
 use Symfony\Component\Messenger\Exception\UnrecoverableMessageHandlingException;
 use webignition\ObjectReflector\ObjectReflector;
 
@@ -41,11 +40,7 @@ class CreateMachineHandlerTest extends AbstractBaseFunctionalTest
     private const MACHINE_ID = 'machine id';
 
     private CreateMachineHandler $handler;
-    private MessengerAsserter $messengerAsserter;
-    private MockHandler $mockHandler;
     private Machine $machine;
-    private MachineStore $machineStore;
-    private MachineProviderStore $machineProviderStore;
 
     protected function setUp(): void
     {
@@ -57,37 +52,23 @@ class CreateMachineHandlerTest extends AbstractBaseFunctionalTest
 
         $machineStore = self::getContainer()->get(MachineStore::class);
         \assert($machineStore instanceof MachineStore);
-        $this->machine = new Machine(self::MACHINE_ID);
+        $this->machine = new Machine(self::MACHINE_ID, Machine::STATE_CREATE_RECEIVED);
         $machineStore->store($this->machine);
 
         $machineProviderStore = self::getContainer()->get(MachineProviderStore::class);
         \assert($machineProviderStore instanceof MachineProviderStore);
         $machineProvider = new MachineProvider(self::MACHINE_ID, ProviderInterface::NAME_DIGITALOCEAN);
         $machineProviderStore->store($machineProvider);
-
-        $messengerAsserter = self::getContainer()->get(MessengerAsserter::class);
-        \assert($messengerAsserter instanceof MessengerAsserter);
-        $this->messengerAsserter = $messengerAsserter;
-
-        $mockHandler = self::getContainer()->get(MockHandler::class);
-        \assert($mockHandler instanceof MockHandler);
-        $this->mockHandler = $mockHandler;
-
-        $machineStore = self::getContainer()->get(MachineStore::class);
-        \assert($machineStore instanceof MachineStore);
-        $this->machineStore = $machineStore;
-
-        $machineProviderStore = self::getContainer()->get(MachineProviderStore::class);
-        \assert($machineProviderStore instanceof MachineProviderStore);
-        $this->machineProviderStore = $machineProviderStore;
     }
 
     public function testInvokeSuccess(): void
     {
         self::assertSame([], ObjectReflector::getProperty($this->machine, 'ip_addresses'));
 
+        $dropletId = 123;
+
         $dropletData = [
-            'id' => 123,
+            'id' => $dropletId,
             'networks' => (object) [
                 'v4' => [
                     (object) [
@@ -104,7 +85,27 @@ class CreateMachineHandlerTest extends AbstractBaseFunctionalTest
         ];
 
         $expectedDropletEntity = new DropletEntity($dropletData);
-        $this->mockHandler->append(HttpResponseFactory::fromDropletEntity($expectedDropletEntity));
+
+        $dropletApiProxy = self::getContainer()->get(DropletProxy::class);
+        if ($dropletApiProxy instanceof DropletProxy) {
+            $dropletConfiguration = $this->createDropletConfiguration('test-worker-machine id');
+
+            $dropletApiProxy->withCreateCall(
+                'test-worker-machine id',
+                $dropletConfiguration->getRegion(),
+                $dropletConfiguration->getSize(),
+                $dropletConfiguration->getImage(),
+                $dropletConfiguration->getBackups(),
+                $dropletConfiguration->getIpv6(),
+                $dropletConfiguration->getVpcUuid(),
+                $dropletConfiguration->getSshKeys(),
+                $dropletConfiguration->getUserData(),
+                $dropletConfiguration->getMonitoring(),
+                $dropletConfiguration->getVolumes(),
+                $dropletConfiguration->getTags(),
+                $expectedDropletEntity,
+            );
+        }
 
         $requestIdFactory = new SequentialRequestIdFactory();
         $machineRequestFactory = new TestMachineRequestFactory(
@@ -120,8 +121,12 @@ class CreateMachineHandlerTest extends AbstractBaseFunctionalTest
         $expectedRequest = $machineRequestFactory->createCheckIsActive(self::MACHINE_ID);
 
         $expectedRemoteMachine = new RemoteMachine($expectedDropletEntity);
-        $this->messengerAsserter->assertQueueCount(1);
-        $this->messengerAsserter->assertMessageAtPositionEquals(0, $expectedRequest);
+
+        $messengerAsserter = self::getContainer()->get(MessengerAsserter::class);
+        \assert($messengerAsserter instanceof MessengerAsserter);
+
+        $messengerAsserter->assertQueueCount(1);
+        $messengerAsserter->assertMessageAtPositionEquals(0, $expectedRequest);
 
         self::assertSame($expectedRemoteMachine->getState(), $this->machine->getState());
         self::assertSame(
@@ -130,24 +135,30 @@ class CreateMachineHandlerTest extends AbstractBaseFunctionalTest
         );
     }
 
-    /**
-     * @dataProvider invokeThrowsExceptionDataProvider
-     */
-    public function testInvokeThrowsException(
-        ?ResponseInterface $httpFixture,
-        Machine $machine,
-        MachineProvider $machineProvider,
-        \Exception $expectedException
-    ): void {
-        if ($httpFixture instanceof ResponseInterface) {
-            $this->mockHandler->append($httpFixture);
+    public function testInvokeUnsupportedProvider(): void
+    {
+        $invalidProvider = 'invalid';
+        $unsupportedProviderException = new UnsupportedProviderException($invalidProvider);
+        $expectedException = new UnrecoverableMessageHandlingException(
+            $unsupportedProviderException->getMessage(),
+            $unsupportedProviderException->getCode(),
+            $unsupportedProviderException
+        );
+
+        $machineProvider = new MachineProvider(self::MACHINE_ID, ProviderInterface::NAME_DIGITALOCEAN);
+        ObjectReflector::setProperty(
+            $machineProvider,
+            MachineProvider::class,
+            'provider',
+            $invalidProvider
+        );
+
+        $machineProviderStore = self::getContainer()->get(MachineProviderStore::class);
+        if ($machineProviderStore instanceof MachineProviderStore) {
+            $machineProviderStore->store($machineProvider);
         }
 
-        $this->machineStore->store($machine);
-        $this->machineProviderStore->store($machineProvider);
-
-        $message = new CreateMachine('id0', $machine->getId());
-        $machineState = $machine->getState();
+        $message = new CreateMachine('id0', $this->machine->getId());
 
         try {
             ($this->handler)($message);
@@ -156,7 +167,45 @@ class CreateMachineHandlerTest extends AbstractBaseFunctionalTest
             self::assertEquals($expectedException, $exception);
         }
 
-        self::assertSame($machineState, $machine->getState());
+        self::assertSame(Machine::STATE_CREATE_REQUESTED, $this->machine->getState());
+    }
+
+    /**
+     * @dataProvider invokeThrowsExceptionDataProvider
+     */
+    public function testInvokeThrowsException(\Exception $vendorException, \Exception $expectedException): void
+    {
+        $dropletApiProxy = self::getContainer()->get(DropletProxy::class);
+        if ($dropletApiProxy instanceof DropletProxy) {
+            $dropletConfiguration = $this->createDropletConfiguration('test-worker-machine id');
+
+            $dropletApiProxy->withCreateCall(
+                'test-worker-machine id',
+                $dropletConfiguration->getRegion(),
+                $dropletConfiguration->getSize(),
+                $dropletConfiguration->getImage(),
+                $dropletConfiguration->getBackups(),
+                $dropletConfiguration->getIpv6(),
+                $dropletConfiguration->getVpcUuid(),
+                $dropletConfiguration->getSshKeys(),
+                $dropletConfiguration->getUserData(),
+                $dropletConfiguration->getMonitoring(),
+                $dropletConfiguration->getVolumes(),
+                $dropletConfiguration->getTags(),
+                $vendorException,
+            );
+        }
+
+        $message = new CreateMachine('id0', $this->machine->getId());
+
+        try {
+            ($this->handler)($message);
+            $this->fail($expectedException::class . ' not thrown');
+        } catch (\Exception $exception) {
+            self::assertEquals($expectedException, $exception);
+        }
+
+        self::assertSame(Machine::STATE_CREATE_REQUESTED, $this->machine->getState());
     }
 
     /**
@@ -164,17 +213,11 @@ class CreateMachineHandlerTest extends AbstractBaseFunctionalTest
      */
     public function invokeThrowsExceptionDataProvider(): array
     {
-        $machine = new Machine(self::MACHINE_ID, Machine::STATE_CREATE_RECEIVED);
-        $machineProvider = new MachineProvider(self::MACHINE_ID, ProviderInterface::NAME_DIGITALOCEAN);
-
         $authenticationException = new AuthenticationException(
             self::MACHINE_ID,
             MachineActionInterface::ACTION_CREATE,
-            new RuntimeException('Unauthorized', 401)
+            new VendorRuntimeException('Unauthorized', 401)
         );
-
-        $invalidProvider = 'invalid';
-        $unsupportedProviderException = new UnsupportedProviderException($invalidProvider);
 
         $unknownRemoteMachineException = new UnknownRemoteMachineException(
             ProviderInterface::NAME_DIGITALOCEAN,
@@ -187,16 +230,14 @@ class CreateMachineHandlerTest extends AbstractBaseFunctionalTest
 
         $apiLimitExceededException = new ApiLimitExceededException(
             $rateLimitReset,
-            $machine->getId(),
+            self::MACHINE_ID,
             MachineActionInterface::ACTION_CREATE,
             new \DigitalOceanV2\Exception\ApiLimitExceededException('Too Many Requests', 429)
         );
 
         return [
             'HTTP 401' => [
-                'httpFixture' => new Response(401),
-                'machine' => $machine,
-                'machineProvider' => $machineProvider,
+                'vendorException' => new VendorRuntimeException('Unauthorized', 401),
                 'expectedException' => new UnrecoverableMessageHandlingException(
                     $authenticationException->getMessage(),
                     $authenticationException->getCode(),
@@ -204,20 +245,11 @@ class CreateMachineHandlerTest extends AbstractBaseFunctionalTest
                 ),
             ],
             'HTTP 404' => [
-                'httpFixture' => new Response(404),
-                'machine' => $machine,
-                'machineProvider' => $machineProvider,
+                'vendorException' => new ResourceNotFoundException('Not Found', 404),
                 'expectedException' => $unknownRemoteMachineException,
             ],
             'HTTP 429' => [
-                'httpFixture' => new Response(
-                    429,
-                    [
-                        'ratelimit-reset' => (string) $rateLimitReset,
-                    ]
-                ),
-                'machine' => $machine,
-                'machineProvider' => $machineProvider,
+                'vendorException' => $apiLimitExceededException,
                 'expectedException' => new UnrecoverableMessageHandlingException(
                     $apiLimitExceededException->getMessage(),
                     $apiLimitExceededException->getCode(),
@@ -225,37 +257,26 @@ class CreateMachineHandlerTest extends AbstractBaseFunctionalTest
                 ),
             ],
             'HTTP 503' => [
-                'httpFixture' => new Response(503),
-                'machine' => $machine,
-                'machineProvider' => $machineProvider,
+                'vendorException' => new VendorRuntimeException('Service Unavailable', 503),
                 'expectedException' => new HttpException(
-                    $machine->getId(),
+                    self::MACHINE_ID,
                     MachineActionInterface::ACTION_CREATE,
-                    new RuntimeException('Service Unavailable', 503)
-                ),
-            ],
-            'Unsupported provider' => [
-                'httpFixture' => null,
-                'machine' => $machine,
-                'machineProvider' => (function () {
-                    $invalidProvider = 'invalid';
-
-                    $machineProvider = new MachineProvider(self::MACHINE_ID, ProviderInterface::NAME_DIGITALOCEAN);
-                    ObjectReflector::setProperty(
-                        $machineProvider,
-                        MachineProvider::class,
-                        'provider',
-                        $invalidProvider
-                    );
-
-                    return $machineProvider;
-                })(),
-                'expectedException' => new UnrecoverableMessageHandlingException(
-                    $unsupportedProviderException->getMessage(),
-                    $unsupportedProviderException->getCode(),
-                    $unsupportedProviderException
+                    new VendorRuntimeException('Service Unavailable', 503)
                 ),
             ],
         ];
+    }
+
+    private function createDropletConfiguration(string $name): Configuration
+    {
+        $factory = self::getContainer()->get(Factory::class);
+        if (false === $factory instanceof Factory) {
+            throw new \RuntimeException(Factory::class . ' service not found');
+        }
+
+        $configuration = $factory->create();
+        $configuration = $configuration->withNames([$name]);
+
+        return $configuration->addTags([$name]);
     }
 }
--- tests/Proxy/DigitalOceanV2/Api/DropletProxy.php
+++ tests/Proxy/DigitalOceanV2/Api/DropletProxy.php
@@ -6,6 +6,7 @@ namespace App\Tests\Proxy\DigitalOceanV2\Api;
 
 use DigitalOceanV2\Api\Droplet;
 use DigitalOceanV2\Client;
+use DigitalOceanV2\Entity\Droplet as DropletEntity;
 use Mockery\MockInterface;
 
 class DropletProxy extends Droplet
@@ -41,8 +42,101 @@ class DropletProxy extends Droplet
         return $this;
     }
 
-    public function getById(int $id)
+    public function getById(int $id): DropletEntity
     {
         return $this->mock->getById($id);
+    }
+
+    /**
+     * @param int[] $sshKeys
+     * @param string[] $volumes
+     * @param string[] $tags
+     */
+    public function withCreateCall(
+        string $name,
+        string $region,
+        string $size,
+        string $image,
+        bool $backups,
+        bool $ipv6,
+        bool|string $vpcUuid,
+        array $sshKeys,
+        string $userData,
+        bool $monitoring,
+        array $volumes,
+        array $tags,
+        object $outcome
+    ): self {
+        if (false === $this->mock instanceof MockInterface) {
+            return $this;
+        }
+
+        $expectation = $this->mock
+            ->shouldReceive('create')
+            ->with(
+                $name,
+                $region,
+                $size,
+                $image,
+                $backups,
+                $ipv6,
+                $vpcUuid,
+                $sshKeys,
+                $userData,
+                $monitoring,
+                $volumes,
+                $tags
+            );
+
+        if ($outcome instanceof \Exception) {
+            $expectation
+                ->andThrow($outcome);
+        } else {
+            $expectation->andReturn($outcome);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param string[]|string $names
+     * @param int[] $sshKeys
+     * @param int[] $volumes
+     * @param int[] $tags
+     */
+    public function create(
+        $names,
+        string $region,
+        string $size,
+        $image,
+        bool $backups = false,
+        bool $ipv6 = false,
+        $vpcUuid = false,
+        array $sshKeys = [],
+        string $userData = '',
+        bool $monitoring = true,
+        array $volumes = [],
+        array $tags = []
+    ): DropletEntity {
+        $droplet = $this->mock->create(
+            $names,
+            $region,
+            $size,
+            $image,
+            $backups,
+            $ipv6,
+            $vpcUuid,
+            $sshKeys,
+            $userData,
+            $monitoring,
+            $volumes,
+            $tags
+        );
+
+        if (false === $droplet instanceof DropletEntity) {
+            throw new \RuntimeException('Created object not of type ' . DropletEntity::class);
+        }
+
+        return $droplet;
     }
 }
