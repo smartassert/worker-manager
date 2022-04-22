c2893f69731878df9ac1be3ec6103404814b0c75
Jon Cram <webignition@gmail.com>
2022-04-22 17:13:23 +0100

    Re-implement machine controller integration unauthorized user tests as application tests

--- tests/Integration/UnauthorizedUserTest.php
+++ tests/Integration/UnauthorizedUserTest.php
@@ -4,66 +4,9 @@ declare(strict_types=1);
 
 namespace App\Tests\Integration;
 
-use App\Controller\MachineController;
-use GuzzleHttp\Exception\ClientException;
+use App\Tests\Application\AbstractUnauthorizedUserTest;
 
-class UnauthorizedUserTest extends AbstractIntegrationTest
+class UnauthorizedUserTest extends AbstractUnauthorizedUserTest
 {
-    private string $machineUrl;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $machineId = md5((string) rand());
-        $this->machineUrl = str_replace('{id}', $machineId, MachineController::PATH_MACHINE);
-    }
-
-    /**
-     * @dataProvider requestAsUnauthorizedUserDataProvider
-     */
-    public function testRequestAsUnauthorizedUser(string $method, ?string $token): void
-    {
-        try {
-            $response = $this->makeRequest($method, $this->machineUrl, $token);
-            self::fail(ClientException::class . ' not thrown');
-        } catch (ClientException $e) {
-            $response = $e->getResponse();
-        }
-
-        self::assertSame(401, $response->getStatusCode());
-    }
-
-    /**
-     * @return array<string, array<string, ?string>>
-     */
-    public function requestAsUnauthorizedUserDataProvider(): array
-    {
-        return [
-            'create, invalid token' => [
-                'method' => 'POST',
-                'token' => 'invalid-token',
-            ],
-            'status, invalid token' => [
-                'method' => 'GET',
-                'token' => 'invalid-token',
-            ],
-            'delete, invalid token' => [
-                'method' => 'DELETE',
-                'token' => 'invalid-token',
-            ],
-            'create, no token' => [
-                'method' => 'POST',
-                'token' => null,
-            ],
-            'status, no token' => [
-                'method' => 'GET',
-                'token' => null,
-            ],
-            'delete, no token' => [
-                'method' => 'DELETE',
-                'token' => null,
-            ],
-        ];
-    }
+    use GetApplicationClientTrait;
 }
