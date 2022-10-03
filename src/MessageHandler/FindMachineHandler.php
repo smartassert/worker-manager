1d6f14b7779481c967f942b50f01eba11a5dde70
Jon Cram <webignition@gmail.com>
2022-10-03 15:20:59 +0100

    Remove use of MachineStore in FindMachineHandler

--- src/MessageHandler/FindMachineHandler.php
+++ src/MessageHandler/FindMachineHandler.php
@@ -12,7 +12,6 @@ use App\Message\FindMachine;
 use App\Model\RemoteMachineInterface;
 use App\Repository\MachineRepository;
 use App\Services\Entity\Store\MachineProviderStore;
-use App\Services\Entity\Store\MachineStore;
 use App\Services\MachineRequestDispatcher;
 use App\Services\MachineUpdater;
 use App\Services\RemoteMachineFinder;
@@ -22,7 +21,6 @@ use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 class FindMachineHandler implements MessageHandlerInterface
 {
     public function __construct(
-        private MachineStore $machineStore,
         private MachineProviderStore $machineProviderStore,
         private RemoteMachineFinder $remoteMachineFinder,
         private MachineUpdater $machineUpdater,
@@ -44,7 +42,7 @@ class FindMachineHandler implements MessageHandlerInterface
         }
 
         $machine->setState(Machine::STATE_FIND_FINDING);
-        $this->machineStore->store($machine);
+        $this->machineRepository->add($machine);
 
         try {
             $remoteMachine = $this->remoteMachineFinder->find($machineId);
@@ -64,7 +62,7 @@ class FindMachineHandler implements MessageHandlerInterface
                 $this->machineRequestDispatcher->dispatchCollection($onSuccessCollection);
             } else {
                 $machine->setState($message->getOnNotFoundState());
-                $this->machineStore->store($machine);
+                $this->machineRepository->add($machine);
 
                 $this->machineRequestDispatcher->dispatchCollection($message->getOnFailureCollection());
             }
