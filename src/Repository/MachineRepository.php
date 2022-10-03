fe71de192569cbe29936fa89ec9be5269fdc06da
Jon Cram <webignition@gmail.com>
2022-10-03 11:32:33 +0100

    Create MachineRepository

---
+++ src/Repository/MachineRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Machine;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Machine>
+ *
+ * @method null|Machine find($id, $lockMode = null, $lockVersion = null)
+ * @method null|Machine findOneBy(array $criteria, array $orderBy = null)
+ * @method Machine[]    findAll()
+ * @method Machine[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class MachineRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Machine::class);
+    }
+
+    public function add(Machine $entity): void
+    {
+        $this->getEntityManager()->persist($entity);
+        $this->getEntityManager()->flush();
+    }
+}
