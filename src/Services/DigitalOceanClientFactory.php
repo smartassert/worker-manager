e4195c265e7d73216efc2c31ebec8f823ac8318b
Jon Cram <webignition@gmail.com>
2022-10-17 17:29:41 +0100

    Create DO client from collection of api tokens

--- src/Services/DigitalOceanClientFactory.php
+++ src/Services/DigitalOceanClientFactory.php
@@ -2,24 +2,66 @@
 
 namespace App\Services;
 
+use App\Exception\NoDigitalOceanClientException;
 use DigitalOceanV2\Client;
+use DigitalOceanV2\Exception\ExceptionInterface;
+use DigitalOceanV2\Exception\RuntimeException;
 use DigitalOceanV2\HttpClient\Builder;
 use Psr\Http\Client\ClientInterface;
+use Psr\Log\LoggerInterface;
 
 class DigitalOceanClientFactory
 {
+    /**
+     * @var array<non-empty-string, non-empty-string>
+     */
+    private readonly array $apiTokens;
+
+    /**
+     * @param array<non-empty-string, non-empty-string> $apiTokens
+     */
     public function __construct(
         private readonly ClientInterface $httpClient,
+        array $apiTokens,
+        private readonly LoggerInterface $logger,
     ) {
+        $filteredApiTokens = [];
+        foreach ($apiTokens as $name => $apiToken) {
+            if ('' !== trim($name) && '' !== trim($apiToken)) {
+                $filteredApiTokens[$name] = $apiToken;
+            }
+        }
+
+        $this->apiTokens = $filteredApiTokens;
     }
 
-    public function create(string $apiToken): Client
+    /**
+     * @throws NoDigitalOceanClientException
+     */
+    public function create(): Client
     {
-        $builder = new Builder($this->httpClient);
-        $client = new Client($builder);
+        foreach ($this->apiTokens as $name => $token) {
+            $client = new Client(new Builder($this->httpClient));
+            $client->authenticate($token);
 
-        $client->authenticate($apiToken);
+            try {
+                $client->droplet()->getById(0);
 
-        return $client;
+                return $client;
+            } catch (ExceptionInterface $exception) {
+                if ($exception instanceof RuntimeException && 404 === $exception->getCode()) {
+                    return $client;
+                }
+
+                $this->logger->error(
+                    $exception->getMessage(),
+                    [
+                        'token_name' => $name,
+                    ]
+                );
+            }
+        }
+
+        throw new NoDigitalOceanClientException();
     }
 }
