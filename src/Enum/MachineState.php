2412f77ec7089869f52895106e1a13ee471c8f87
Jon Cram <webignition@gmail.com>
2022-12-01 15:52:43 +0000

    Add state groups to Enum\MachineState

--- src/Enum/MachineState.php
+++ src/Enum/MachineState.php
@@ -18,4 +18,23 @@ enum MachineState: string
     case DELETE_REQUESTED = 'delete/requested';
     case DELETE_FAILED = 'delete/failed';
     case DELETE_DELETED = 'delete/deleted';
+
+    public const PRE_ACTIVE_STATES = [
+        self::CREATE_RECEIVED,
+        self::CREATE_REQUESTED,
+        self::UP_STARTED,
+    ];
+
+    public const END_STATES = [
+        self::CREATE_FAILED,
+        self::DELETE_FAILED,
+        self::DELETE_DELETED,
+        self::FIND_NOT_FINDABLE,
+        self::FIND_NOT_FOUND,
+    ];
+
+    public const RESETTABLE_STATES = [
+        self::FIND_NOT_FOUND,
+        self::CREATE_FAILED,
+    ];
 }
