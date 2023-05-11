37822d5ae2ba055439a391233487e65e1e8b9701
Jon Cram <webignition@gmail.com>
2023-05-11 16:53:46 +0100

    Remove unused cases

--- src/Enum/MachineAction.php
+++ src/Enum/MachineAction.php
@@ -7,7 +7,5 @@ enum MachineAction: string
     case CREATE = 'create';
     case GET = 'get';
     case DELETE = 'delete';
-    case EXISTS = 'exists';
     case FIND = 'find';
-    case CHECK_IS_ACTIVE = 'check_is_active';
 }
