4fceb3007444f088aa2ebfaa5629d401b320e7a8
Jon Cram <webignition@gmail.com>
2023-08-04 17:39:47 +0100

    Remove ProviderMachineNotFoundException::getProviderName()

--- src/Exception/MachineProvider/ProviderMachineNotFoundException.php
+++ src/Exception/MachineProvider/ProviderMachineNotFoundException.php
@@ -10,22 +10,8 @@ class ProviderMachineNotFoundException extends AbstractMachineException
      * @param non-empty-string $id
      * @param non-empty-string $providerName
      */
-    public function __construct(
-        string $id,
-        private string $providerName,
-    ) {
-        parent::__construct(
-            $id,
-            sprintf(
-                'Machine "%s" not found with provider "%s"',
-                $id,
-                $providerName
-            )
-        );
-    }
-
-    public function getProviderName(): string
+    public function __construct(string $id, string $providerName)
     {
-        return $this->providerName;
+        parent::__construct($id, sprintf('Machine "%s" not found with provider "%s"', $id, $providerName));
     }
 }
