c59e853041bf959546aa516a60380b2f26371930
Jon Cram <webignition@gmail.com>
2025-01-07 11:04:19 +0000

    Create NetworkCollection model (#594)

---
+++ src/Services/MachineManager/DigitalOcean/Entity/NetworkCollection.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services\MachineManager\DigitalOcean\Entity;
+
+readonly class NetworkCollection
+{
+    /**
+     * @param Network[] $networks
+     */
+    public function __construct(
+        public array $networks,
+    ) {
+    }
+
+    /**
+     * @return non-empty-string[]
+     */
+    public function getPublicIpv4Addresses(): array
+    {
+        $ipAddresses = [];
+
+        foreach ($this->networks as $network) {
+            $ipAddress = $network->getPublicIpv4Address();
+
+            if (is_string($ipAddress)) {
+                $ipAddresses[] = $ipAddress;
+            }
+        }
+
+        return $ipAddresses;
+    }
+
+    /**
+     * @return ?non-empty-string
+     */
+    public function getFirstPublicIpv4Address(): ?string
+    {
+        return $this->getPublicIpv4Addresses()[0] ?? null;
+    }
+}
---
+++ src/Services/MachineManager/DigitalOcean/EntityFactory/NetworkCollectionFactory.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Services\MachineManager\DigitalOcean\EntityFactory;
+
+use App\Services\MachineManager\DigitalOcean\Entity\Network;
+use App\Services\MachineManager\DigitalOcean\Entity\NetworkCollection;
+use App\Services\MachineManager\DigitalOcean\Exception\InvalidEntityDataException;
+
+readonly class NetworkCollectionFactory
+{
+    public function __construct(
+        private NetworkFactory $networkFactory,
+    ) {
+    }
+
+    /**
+     * @param array<mixed> $data
+     *
+     * @throws InvalidEntityDataException
+     */
+    public function create(array $data): NetworkCollection
+    {
+        $networks = [];
+
+        $v4Data = $data['v4'] ?? [];
+        $networks = array_merge($networks, $this->createFromSet(is_array($v4Data) ? $v4Data : [], 4));
+
+        $v6Data = $data['v6'] ?? [];
+        $networks = array_merge($networks, $this->createFromSet(is_array($v6Data) ? $v6Data : [], 6));
+
+        return new NetworkCollection($networks);
+    }
+
+    /**
+     * @param array<mixed> $data
+     * @param positive-int $ipVersion
+     *
+     * @return Network[]
+     *
+     * @throws InvalidEntityDataException
+     */
+    private function createFromSet(array $data, int $ipVersion): array
+    {
+        $networks = [];
+
+        foreach ($data as $networkData) {
+            if (is_array($networkData)) {
+                $networks[] = $this->networkFactory->create($networkData, $ipVersion);
+            }
+        }
+
+        return $networks;
+    }
+}
