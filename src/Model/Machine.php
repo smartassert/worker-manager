4ea80bc2d6217ca8d5ba3fd3baca9bc7e65d95bd
Jon Cram <webignition@gmail.com>
2026-05-20 11:40:59 +0100

    Re-implement MachineResponse as serializable model

---
+++ src/Model/Machine.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model;
+
+use App\Entity\ActionFailure;
+use App\Entity\Machine as MachineEntity;
+use App\Enum\MachineState;
+use App\Enum\MachineStateCategory;
+
+readonly class Machine implements \JsonSerializable
+{
+    public function __construct(
+        private MachineEntity $machine,
+        private ?ActionFailure $actionFailure = null
+    ) {}
+
+    /**
+     * @return array{
+     *     id: non-empty-string,
+     *     state: MachineState,
+     *     ip_addresses: string[],
+     *     state_category: MachineStateCategory,
+     *     action_failure: ?ActionFailure,
+     *     has_failed_state: bool,
+     *     has_active_state: bool,
+     *     has_ending_state: bool,
+     *     has_end_state: bool,
+     *     meta_state: array{
+     *       ended: bool,
+     *       succeeded: bool
+     *     }
+     * }
+     */
+    public function jsonSerialize(): array
+    {
+        $stateCategory = $this->machine->getStateCategory();
+
+        $hasFailedState = $this->machine->hasFailedState();
+        $hasEndState = MachineStateCategory::END === $stateCategory;
+        $hasSucceeded = $hasEndState && false === $hasFailedState;
+
+        return [
+            'id' => $this->machine->getId(),
+            'state' => $this->machine->getState(),
+            'ip_addresses' => $this->machine->getIpAddresses(),
+            'state_category' => $stateCategory,
+            'action_failure' => $this->actionFailure,
+            'has_failed_state' => $this->machine->hasFailedState(),
+            'has_active_state' => MachineStateCategory::ACTIVE === $stateCategory,
+            'has_ending_state' => MachineStateCategory::ENDING === $stateCategory,
+            'has_end_state' => $hasEndState,
+            'meta_state' => [
+                'ended' => $hasEndState,
+                'succeeded' => $hasSucceeded,
+            ],
+        ];
+    }
+}
