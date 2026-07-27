a13c2aacdf15348582a16ddbb4c1a4ef00ffe1d1
Jon Cram <webignition@gmail.com>
2026-07-27 16:11:52 +0100

    Create message

---
+++ src/Message/FindMachineAfterDeletion.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Message;
+
+use App\Enum\MachineAction;
+use App\Enum\MachineState;
+
+class FindMachineAfterDeletion extends AbstractRemoteMachineRequest implements MachineActionInterface
+{
+    public function getAction(): MachineAction
+    {
+        return MachineAction::FIND;
+    }
+
+    public function getFailureState(): MachineState
+    {
+        return MachineState::FIND_NOT_FINDABLE;
+    }
+}
