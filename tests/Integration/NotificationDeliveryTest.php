16c05009d0b49d7ec43ce2605f717b2d4af96a26
Jon Cram <webignition@gmail.com>
2026-09-02 17:12:04 +0100

    Verify for machine deletion

--- tests/Integration/NotificationDeliveryTest.php
+++ tests/Integration/NotificationDeliveryTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Tests\Integration;
 
 use App\Entity\Machine;
+use App\Enum\MachineState;
 use App\Repository\MachineRepository;
 use App\Tests\Application\AbstractMachineTestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
@@ -109,6 +110,234 @@ class NotificationDeliveryTest extends AbstractMachineTestCase
                                 'pending' => false,
                                 'ended' => true,
                                 'succeeded' => false,
+                            ],
+                            'previous_states' => [
+                                'unknown',
+                                'find/received',
+                                'find/finding',
+                                'create/received',
+                                'create/requested',
+                                'up/started',
+                                'up/active',
+                                'delete/received',
+                                'delete/requested',
+                            ],
+                        ],
+                    ];
+                },
+            ],
+        ];
+    }
+
+    /**
+     * @param callable(string, string, MachineRepository): void $machineSetup
+     * @param callable(Machine): array<array<mixed>>            $expectedRequestBodiesCreator
+     */
+    #[DataProvider('machineStateChangeNotificationsForMachineDeletionDataProvider')]
+    public function testMachineStateChangeNotificationsForMachineDeletion(
+        callable $machineSetup,
+        int $expectedDispatchedNotificationsCount,
+        callable $expectedRequestBodiesCreator,
+    ): void {
+        $machineRepository = self::getContainer()->get(MachineRepository::class);
+        \assert($machineRepository instanceof MachineRepository);
+
+        $machineId = (string) new Ulid();
+        $notifyUrl = 'http://callback-receiver:8080';
+        $stopState = 'delete/failed';
+
+        $machineSetup($machineId, $notifyUrl, $machineRepository);
+
+        $this->makeValidDeleteRequest($machineId, $notifyUrl);
+
+        $machine = $machineRepository->find($machineId);
+        self::assertInstanceOf(Machine::class, $machine);
+
+        new WaitFor()->waitFor(
+            30,
+            function () use ($machineId, $stopState) {
+                $jobState = $this->getMachineState($machineId);
+
+                return $stopState === $jobState;
+            },
+            $machineId . '" to be in "' . $stopState . '"',
+        );
+
+        $process = Process::fromShellCommandline('docker logs callback-receiver');
+        $process->run();
+
+        $output = $process->getOutput();
+        $parser = new Parser();
+
+        $requests = $parser->parse($output, $expectedDispatchedNotificationsCount);
+        self::assertCount($expectedDispatchedNotificationsCount, $requests);
+
+        $expectedRequestBodies = $expectedRequestBodiesCreator($machine);
+
+        foreach ($expectedRequestBodies as $requestIndex => $expectedRequestBody) {
+            $request = $requests[$requestIndex];
+
+            self::assertSame('POST', $request->getMethod());
+            self::assertSame('application/json', $request->getHeaderLine('Content-Type'));
+            self::assertEquals('/worker-manager.machine.state_changed', (string) $request->getUri());
+
+            $requestData = json_decode($request->getBody()->getContents(), true);
+            self::assertIsArray($requestData);
+            self::assertEquals($expectedRequestBody, $requestData);
+        }
+    }
+
+    /**
+     * @return array<mixed>
+     */
+    public static function machineStateChangeNotificationsForMachineDeletionDataProvider(): array
+    {
+        return [
+            'no pre-existing machine' => [
+                'machineSetup' => function () {},
+                'expectedDispatchedNotificationsCount' => 3,
+                'expectedRequestBodiesCreator' => function (Machine $machine) {
+                    return [
+                        [
+                            'id' => $machine->getId(),
+                            'state' => 'delete/received',
+                            'ip_addresses' => [],
+                            'state_category' => 'ending',
+                            'action_failure' => null,
+                            'has_active_state' => false,
+                            'has_ending_state' => true,
+                            'meta_state' => [
+                                'pending' => false,
+                                'ended' => false,
+                                'succeeded' => false,
+                            ],
+                            'previous_states' => [
+                                'find/received',
+                                'find/finding',
+                                'create/received',
+                                'create/requested',
+                                'up/started',
+                                'up/active',
+                            ],
+                        ],
+                        [
+                            'id' => $machine->getId(),
+                            'state' => 'delete/requested',
+                            'ip_addresses' => [],
+                            'state_category' => 'ending',
+                            'action_failure' => null,
+                            'has_active_state' => false,
+                            'has_ending_state' => true,
+                            'meta_state' => [
+                                'pending' => false,
+                                'ended' => false,
+                                'succeeded' => false,
+                            ],
+                            'previous_states' => [
+                                'find/received',
+                                'find/finding',
+                                'create/received',
+                                'create/requested',
+                                'up/started',
+                                'up/active',
+                                'delete/received',
+                            ],
+                        ],
+                        [
+                            'id' => $machine->getId(),
+                            'state' => 'delete/failed',
+                            'ip_addresses' => [],
+                            'state_category' => 'end',
+                            'action_failure' => null,
+                            'has_active_state' => false,
+                            'has_ending_state' => false,
+                            'meta_state' => [
+                                'pending' => false,
+                                'ended' => true,
+                                'succeeded' => true,
+                            ],
+                            'previous_states' => [
+                                'unknown',
+                                'find/received',
+                                'find/finding',
+                                'create/received',
+                                'create/requested',
+                                'up/started',
+                                'up/active',
+                                'delete/received',
+                                'delete/requested',
+                            ],
+                        ],
+                    ];
+                },
+            ],
+            'has pre-existing machine' => [
+                'machineSetup' => function (string $machineId, string $notifyUrl, MachineRepository $machineRepository) {
+                    $machine = new Machine($machineId, $notifyUrl);
+                    $machine->setState(MachineState::UP_ACTIVE);
+
+                    $machineRepository->add($machine);
+                },
+                'expectedDispatchedNotificationsCount' => 3,
+                'expectedRequestBodiesCreator' => function (Machine $machine) {
+                    return [
+                        [
+                            'id' => $machine->getId(),
+                            'state' => 'delete/received',
+                            'ip_addresses' => [],
+                            'state_category' => 'ending',
+                            'action_failure' => null,
+                            'has_active_state' => false,
+                            'has_ending_state' => true,
+                            'meta_state' => [
+                                'pending' => false,
+                                'ended' => false,
+                                'succeeded' => false,
+                            ],
+                            'previous_states' => [
+                                'find/received',
+                                'find/finding',
+                                'create/received',
+                                'create/requested',
+                                'up/started',
+                                'up/active',
+                            ],
+                        ],
+                        [
+                            'id' => $machine->getId(),
+                            'state' => 'delete/requested',
+                            'ip_addresses' => [],
+                            'state_category' => 'ending',
+                            'action_failure' => null,
+                            'has_active_state' => false,
+                            'has_ending_state' => true,
+                            'meta_state' => [
+                                'pending' => false,
+                                'ended' => false,
+                                'succeeded' => false,
+                            ],
+                            'previous_states' => [
+                                'find/received',
+                                'find/finding',
+                                'create/received',
+                                'create/requested',
+                                'up/started',
+                                'up/active',
+                                'delete/received',
+                            ],
+                        ],
+                        [
+                            'id' => $machine->getId(),
+                            'state' => 'delete/failed',
+                            'ip_addresses' => [],
+                            'state_category' => 'end',
+                            'action_failure' => null,
+                            'has_active_state' => false,
+                            'has_ending_state' => false,
+                            'meta_state' => [
+                                'pending' => false,
+                                'ended' => true,
+                                'succeeded' => true,
                             ],
                             'previous_states' => [
                                 'unknown',
