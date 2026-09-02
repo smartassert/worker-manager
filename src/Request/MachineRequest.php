e34b2b2c6e2e99ba95fc6e81e4616f6626cd7e16
Jon Cram <webignition@gmail.com>
2026-09-02 12:25:41 +0100

    Find/delete machine using custom request

--- src/Request/MachineRequest.php
+++ src/Request/MachineRequest.php
@@ -2,7 +2,7 @@
 
 namespace App\Request;
 
-class CreateMachineRequest
+class MachineRequest
 {
     public const string KEY_ID = 'id';
 
--- src/ValueResolver/MachineRequestResolver.php
+++ src/ValueResolver/MachineRequestResolver.php
@@ -4,23 +4,23 @@ declare(strict_types=1);
 
 namespace App\ValueResolver;
 
-use App\Request\CreateMachineRequest;
+use App\Request\MachineRequest;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 
-final readonly class CreateMachineRequestResolver implements ValueResolverInterface
+final readonly class MachineRequestResolver implements ValueResolverInterface
 {
     /**
-     * @return CreateMachineRequest[]
+     * @return MachineRequest[]
      */
     public function resolve(Request $request, ArgumentMetadata $argument): iterable
     {
-        if ('POST' !== $request->getMethod() || CreateMachineRequest::class !== $argument->getType()) {
+        if (MachineRequest::class !== $argument->getType()) {
             return [];
         }
 
-        $id = $request->attributes->get(CreateMachineRequest::KEY_ID);
+        $id = $request->attributes->get(MachineRequest::KEY_ID);
         $id = is_string($id) ? trim($id) : null;
         $id = '' === $id ? null : $id;
 
@@ -28,6 +28,6 @@ final readonly class CreateMachineRequestResolver implements ValueResolverInterf
             return [];
         }
 
-        return [new CreateMachineRequest($id)];
+        return [new MachineRequest($id)];
     }
 }
